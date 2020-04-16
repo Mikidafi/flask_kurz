14f7d7187882c89201e807739e3d9ab636b7795b
mikidafi <mchl.str84@gmail.com>
2020-04-16 21:01:58 +0000

    using jinja commit system

---
+++ mdblog/database.py
@@ -0,0 +1,21 @@
+articles = {
+	1 : {
+		"title" : "Hello world",
+		"content" : "Cupcake ipsum dolor sit amet wafer chocolate cake macaroon. Pudding bear claw sweet marshmallow pie liquorice lollipop chupa chups. Oat cake tootsie roll lollipop tart. Lollipop jelly tart toffee. Liquorice chocolate chocolate bar candy chocolate cake candy canes gummies pie. Jelly-o candy gingerbread."
+		},
+
+	2 : {
+		"title" : "Hello world",
+		"content" : "Cupcake ipsum dolor sit amet wafer chocolate cake macaroon. Pudding bear claw sweet marshmallow pie liquorice lollipop chupa chups. Oat cake tootsie roll lollipop tart. Lollipop jelly tart toffee. Liquorice chocolate chocolate bar candy chocolate cake candy canes gummies pie. Jelly-o candy gingerbread."
+		},
+
+	3 : {
+		"title" : "Hello world",
+		"content" : "Cupcake ipsum dolor sit amet wafer chocolate cake macaroon. Pudding bear claw sweet marshmallow pie liquorice lollipop chupa chups. Oat cake tootsie roll lollipop tart. Lollipop jelly tart toffee. Liquorice chocolate chocolate bar candy chocolate cake candy canes gummies pie. Jelly-o candy gingerbread."
+		},
+
+	4 : {
+		"title" : "Hello world",
+		"content" : "Cupcake ipsum dolor sit amet wafer chocolate cake macaroon. Pudding bear claw sweet marshmallow pie liquorice lollipop chupa chups. Oat cake tootsie roll lollipop tart. Lollipop jelly tart toffee. Liquorice chocolate chocolate bar candy chocolate cake candy canes gummies pie. Jelly-o candy gingerbread."
+		}
+		}
